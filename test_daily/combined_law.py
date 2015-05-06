c513c3875025e70a0ad61458b454195cfc477162
arwen <74552925@qq.com>
2015-05-06 10:17:04 +0800

    py

---
+++ test_daily/combined_law.py
@@ -0,0 +1,10 @@
+#/usr/bin/python
+#Filename combined_law.py
+
+length = 5
+breadth = 2
+area = length*breadth
+print'Areais', area
+print 'Perimeter is' , 2*(length+breadth)
+
+
---
+++ test_daily/control_flow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#Filename: control_flow.py
+
+running  = True
+number = 23
+while running:
+	guess = int(raw_input('Enter an integer:'))
+	if guess == number:
+		print'Congratulation, you guessed it.'
+		print"(but you donot win any prizes!)"
+	elif guess < number:
+		print'No, it is a little higher than that'
+	else:
+		print'No, itis a litte lower than that'
+
+print'Done'
+
